0c13f7e47bf6ff12cf49337ba0602c590fff1d56
sivanesh1996 <vssivanesh@gmail.com>
2022-08-26 18:16:46 +0530

    maximum of 3 numbers using if

---
+++ Max3if.java
@@ -0,0 +1,28 @@
+class Max3if
+{
+    public static void main(String[] args)
+     {
+      int a=41;
+      int b=19;
+      int c=9;
+      int d;
+        if(a>=b)
+          {
+            d=a;
+          }
+        else
+          {
+            d=b;
+           }
+         if(d>=c)
+           {
+            d=d;    
+            }
+          else
+            {
+             d=c;
+            }
+    
+      System.out.println(d);
+     }
+}
