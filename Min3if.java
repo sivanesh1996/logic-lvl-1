5520d60eec95799e1a1194e021015dc86a3f8116
sivanesh1996 <vssivanesh@gmail.com>
2022-08-27 15:40:28 +0530

    Minimum of 3 numbers using if

---
+++ Min3if.java
@@ -0,0 +1,28 @@
+class Min3if
+{
+    public static void main(String[] args)
+     {
+      int a=91;
+      int b=98;
+      int c=99;
+      int d;
+        if(a<=b)
+          {
+            d=a;
+          }
+        else
+          {
+            d=b;
+           }
+         if(d<=c)
+           {
+            d=d;    
+            }
+          else
+            {
+             d=c;
+            }
+    
+      System.out.println(d);
+     }
+}
