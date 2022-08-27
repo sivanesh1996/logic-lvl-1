e8f00f897a606adeb4f24890d371170176aa7ddb
sivanesh1996 <vssivanesh@gmail.com>
2022-08-27 15:36:06 +0530

    Minimum of 2 numbers using if

---
+++ Min2if.java
@@ -0,0 +1,18 @@
+class Min2if
+{
+    public static void main(String[] args)
+     {
+      int a=14;
+      int b=4;
+      int c;
+        if(a<=b)
+          {
+            c=a;
+          }
+         else
+          {
+            c=b;
+           }
+      System.out.println(c);
+     }
+}
