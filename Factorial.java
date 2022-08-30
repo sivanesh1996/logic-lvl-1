3ce763e3b8cf3c4641d35087ec42ff7a6cd6b244
sivanesh1996 <vssivanesh@gmail.com>
2022-08-30 10:57:35 +0530

    finding factorial

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+class Factorial
+{
+    public static void main(String[] args)
+    {
+     int n=7;
+     int fact=1;
+     for(;n>=1;n--)
+     {
+      fact=fact*n;
+     }
+    System.out.println(fact);
+    }
+}
