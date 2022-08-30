7648d32bf14ad0f94d5be03d462e3493c550b8bf
sivanesh1996 <vssivanesh@gmail.com>
2022-08-30 10:56:52 +0530

    basic-adding digits

---
+++ AddDigits.java
@@ -0,0 +1,15 @@
+class AddDigits
+{
+    public static void main(String[] args)
+    {
+     int num=9999999;
+     int sum=0;
+     while(num>0)
+        {
+         sum=sum+num%10;
+         num=num/10;
+         }  
+     System.out.println(sum);
+    
+   }
+}
